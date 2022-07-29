fefb0078b23e9d5f872b54b3fe98ce8fd6c85678
Beaaninhar <aninhabeatrizribeiro@gmail.com>
2022-07-29 14:17:47 -0300

    refatoramento inicial

--- tIdosos_api/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("tidosos", "12345", {
-  host: "localhost",
-  dialect: "postgres",
-});
-
-module.exports = sequelize;
--- tIdosos_api/src/server.js
+++ tIdosos_api/src/server.js
@@ -1,29 +1,5 @@
-const morganBody = require("morgan-body");
 const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const path = require("path");
-const moment = require("moment");
-const routes = require("./routes");
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(routes);
-
-// *** Configuração do log
-const log = fs.createWriteStream(
-  path.join(
-    __dirname,
-    "./logs",
-    `server.js-${moment().format("YYYY-MM-DD")}.log`
-  ),
-  { flags: "a" }
-);
-
-morganBody(app, {
-  noColors: true,
-  stream: log,
-});
 
 app.listen(4001, () => console.log("Servidor rodando na porta 4001!"));
