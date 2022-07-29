f81090462784f8617c307faabf5cd1a405474a17
Beaaninhar <aninhabeatrizribeiro@gmail.com>
2022-07-29 16:10:10 -0300

    API: feito conexão com o banco e criando a primeira migration

--- tIdosos_api/src/config/database.js
+++ tIdosos_api/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'api_tidosos',
-  password: '12345',
-  database: 'sqlnode',
+  dialect: "postgres",
+  host: "localhost",
+  username: "tidosos",
+  password: "12345",
+  database: "sqlnode",
   define: {
     timestamps: true,
     underscored: true,
