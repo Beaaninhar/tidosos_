a48e77ed5dc0b31f0295c481edaf3a4b8bf41c04
Beaaninhar <aninhabeatrizribeiro@gmail.com>
2022-07-29 14:56:42 -0300

    refatoramento inicial

---
+++ tIdosos_api/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'api_tidosos',
+  password: '12345',
+  database: 'sqlnode',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
