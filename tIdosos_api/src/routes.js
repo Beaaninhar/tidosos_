d02f966d1f5104e6a5bd8d194b20ddf807a2705c
Beaaninhar <aninhabeatrizribeiro@gmail.com>
2022-08-26 10:32:00 -0300

    Inclusão e refatoramento do docker

--- tIdosos_api/src/routes.js
+++ tIdosos_api/src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ data: "Hello Word! dockerizado DE VERDADE!!!!!" });
+  return res.json({ data: "Hello Word" });
 });
 
 module.exports = routes;
